9f3ec2f31f1fa0e7ea97b67fad9392d6bbd7559f
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-03-01 23:37:01 +0600

    footer

--- portafule/src/components/Footer.jsx
+++ portafule/src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      footer
+      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        footer here
+      </div>
     </div>
   )
 }
