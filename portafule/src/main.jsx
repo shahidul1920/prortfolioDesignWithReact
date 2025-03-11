fe26c547ae4d661ebdd5c576a96fdf5817892fd0
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-03-11 21:05:14 +0600

    lanis added

--- portafule/src/main.jsx
+++ portafule/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ReactLenis } from "lenis/dist/lenis-react";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ReactLenis root options={{
+      lerp: 0.05,
+    }}>
+      <App />
+    </ReactLenis>
+
+    {/* <App /> */}
   </StrictMode>,
 )
